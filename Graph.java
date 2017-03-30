5750e505dbf579c8b32428ce343de03d09df1b9e
qdvu124 <vudinhquan124@gmail.com>
2017-03-30 12:17:23 -0400

    Anotated code

--- Graph.java
+++ Graph.java
@@ -31,35 +31,48 @@ class Graph
 
     public boolean IsConnected()
     {
+    	// Perform BFS to check if the graph is connected
+    	// Set to keep track of visited nodes
         HashSet<Integer> visited = new HashSet<Integer>();
+        // Keeping track of the current node and the next node from the adjacency list
         int current, next;
+        // Queue to perform BFS
         LinkedList<Integer> queue = new LinkedList<Integer>();
+        // Adding an arbitrary start point. Which point doesn't matter since if the graph is connected, all nodes will be added to the visited set
         queue.add(0);
+        // Iterator for each list
         Iterator<Integer> iter;
         while(!queue.isEmpty()) {
-          current = queue.remove();
-          if(!visited.contains(current))
-            visited.add(current);
-          iter = adjList[current].iterator();
-          while(iter.hasNext()) {
-            next = iter.next();
-            if(visited.contains(next) || queue.contains(next))
-              continue;
-            queue.add(next);
-          }
+		// Still unvisited nodes in the queue
+		  current = queue.remove();
+		  // Adding the node to the list of visited nodes
+		  if(!visited.contains(current))
+			visited.add(current);
+		  // Adding the neighbors of the current node to the queue
+		  iter = adjList[current].iterator();
+		  while(iter.hasNext()) {
+			next = iter.next();
+			// Only if the neighbor has not been visited or if the neighbor is not yet in the queue
+			if(visited.contains(next) || queue.contains(next))
+			  continue;
+			queue.add(next);
+		  }
         }
         return visited.size() == numVertices;
     }
 
     public boolean IsCycle()
     {
+    	// Automatically fail if condition one is not met
     	if(!IsConnected())
     		return false;
+    	// Check that the sizes of all lists are exactly 2
     	for(int i = 0; i < numVertices; i++) {
     		if(adjList[i].size() != 2) {
     			return false;
     		}
     	}
+    	// Return true since both conditions are met
         return true;
     }
 }
